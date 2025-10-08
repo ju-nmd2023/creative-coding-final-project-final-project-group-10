ac5dcb62696240002bbff4a64047f2214316f9fb
0genina0 <geni23cs@student.ju.se>
2025-10-08 21:29:38 +0200

    finished day and night screen

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
+let isNight = false;
+
+const size = 250;
+const layers = 120;
+
+const gridCols = 8;
+const gridRows = 4;
+
+let position; 
+
 function setup(){
     createCanvas(innerWidth, innerHeight);
     position = createVector(100,100);
+
+    let button = createButton('Toggle Day/Night');
+
+    button.position (20,20);
+
+    button.mousePressed(toggleDayNight); 
+
+    frameRate(2);
 }
 
-    const size = 150;
-    const layers = 10;
+function toggleDayNight() {
+    isNight = !isNight;
+  }
+  
 
     function getRandomValue(pos, variance){
         return pos + random(-variance, variance);
     }
 
-    function drawLayers(x,y,size,layers, isDay = true){
+    function drawLayers(x,y,size,layers, isNight){
 
         noFill();
-        stroke(14,108,217, 50);
-        strokeWeight(3);
+        strokeWeight(2);
     
-        const variance = size / 50;
+        const variance = isNight ? size / 2: size /150;
     
         for(let i = 0; i < layers; i++){
             const s = (size / layers) * i;
-            const half = s / 2;
+            const half = isNight ? s / 100: s / 2;
           
             beginShape();
             vertex(getRandomValue(x - half, variance), getRandomValue( y - half, variance));
@@ -34,25 +53,42 @@ function setup(){
     }
 
 
-function draw(){
-    stroke(14,108,217, 50);
-    background(174,198,207);
-
-    if (isDay = true){
-    for (let y = 0; y < 10; y++){
-        for (let x = 0; x < 10; x ++){
-        const centerX = size / 50 + x * size;
-        const centerY = size / 50 + y *size;
-
-        drawLayers(centerX, centerY, size, layers);  
-     }
+    function draw(){
+    
+        const totalGridWidth = gridCols * size;
+        const totalGridHeight = gridRows * size;
+        const startX = (width - totalGridWidth) / 2;
+        const startY = (height - totalGridHeight) / 2;
+    
+        if (!isNight){ 
+            // DAY MODE
+            stroke(174,198,207);
+            background(255, 255,255);
+         
+    
+            for (let y = 0; y < gridRows; y++){
+                for (let x = 0; x < gridCols; x ++){
+                    const centerX = startX + x * size + size / 2;
+                    const centerY = startY + y * size + size / 2;
+                    drawLayers(centerX, centerY, size, layers, isNight);  
+                }
+            }
+        } else {
+            // NIGHT MODE
+            stroke(8, 15, 40); 
+            background (4, 7, 32);
+            
+            for (let y = 0; y < gridRows; y++){
+                for (let x = 0; x < gridCols; x ++){
+                    const centerX = startX + x * size + size / 2;
+                    const centerY = startY + y * size + size / 2;
+                    drawLayers(centerX, centerY, size, layers, isNight);  
+                }
+            }
+        
+        }
     }
-} else {
-    background (255);
-}
 
-    noLoop();
-
-}
+    
 
 //change square variance for night so they look liuke stars
